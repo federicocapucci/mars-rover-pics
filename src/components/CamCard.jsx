83aaa07c11b24696d7d39c4513af583de7f2ec87
Corxario <federicocapucci@hotmail.com>
2022-11-07 10:02:29 -0300

    add style to cam filter

--- src/components/CamCard.jsx
+++ src/components/CamCard.jsx
@@ -3,11 +3,14 @@ import PropTypes from 'prop-types'
 
 const CamCard = ({ cam, filterPics }) => {
   return (
-    <div className="mt-1 mb-3 mx-1">
+    <div className="mt-1 mb-2 mx-1">
       <a
         className={
-          cam === 'Clear Filter' ? 'btn btn-success' : 'btn btn-secondary'
+          cam === 'Clear Filter'
+            ? 'btn btn-success solp textBorder'
+            : 'btn btn-secondary solp textBorder'
         }
+        style={{ fontSize: '0.5rem', color: 'rgba(255,255,255,0.9)' }}
         rel="noreferrer"
         onClick={() => filterPics(cam)}
       >
